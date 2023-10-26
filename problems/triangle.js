7df4d2f50ba5b166501a0f434729f6da8c256f9e
Thanh Pham <jonathanphamw22@gmail.com>
2023-10-26 16:40:13 -0700

    completed triangle-spec

--- problems/triangle.js
+++ problems/triangle.js
@@ -1,1 +1,24 @@
-// Your code here 
+class Triangle {
+    constructor(side1, side2, side3){
+        this.side1 = side1
+        this.side2 = side2
+        this.side3 = side3
+
+    }
+    getPerimeter() {
+        return this.side1 + this.side2 + this.side3;
+    }
+    
+    hasValidSideLengths() {
+        return (this.side1 + this.side2 ) > this.side3
+    }
+
+    validate(){
+        
+        if(this.hasValidSideLengths) this.isValid = true;
+        else this.isValid = false
+    }
+
+}
+
+module.exports = Triangle
--- test/triangle-spec.js
+++ test/triangle-spec.js
@@ -1,1 +1,33 @@
-// Your code here 
+const chai = require("chai");
+const expect = chai.expect;
+const spies = require("chai-spies");
+chai.use(spies);
+const Triangle = require("../problems/triangle")
+let newTriangle = new Triangle (3, 4, 5)
+
+
+describe("Constructor requirement",  () => {
+    it ("Should have side1, side2, side3", () => {
+        expect(newTriangle.side1).to.equal(3)
+        expect(newTriangle.side2).to.equal(4)
+        expect(newTriangle.side3).to.equal(5)
+    } )
+})
+describe("Get perimeter", () => {
+    it("Should get the perimeter", () => {
+        expect(newTriangle.getPerimeter()).to.equal(12)
+    })
+    
+})
+describe("Should have valid side length", () => {
+    it("Has valid side", () => {
+        expect(newTriangle.hasValidSideLengths()).to.be.true
+       
+    })})
+
+    describe("Validate", () => {
+        it("Valid triangle has valid property", () => {
+            expect(newTriangle.isValid).to.be.true
+        })
+
+    } )
