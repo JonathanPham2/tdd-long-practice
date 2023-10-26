1150be362c172e3cc812a4763e0b53606cb50e4e
Thanh Pham <jonathanphamw22@gmail.com>
2023-10-26 15:20:19 -0700

    completed phase 2

--- test/number-fun-spec.js
+++ test/number-fun-spec.js
@@ -1,1 +1,18 @@
-// Your code here 
+const chai = require("chai");
+const expect = chai.expect;
+const spies = require("chai-spies");
+chai.use(spies);
+const {returnsThree, reciprocal} = require ("../problems/number-fun")
+describe ("Return three function", function () {
+    it ("Should return three", function () {
+        expect(returnsThree()).to.equal(3)
+    })
+} 
+
+)
+describe("Should return reciprocal", function () {
+    it ("Should return reciprocal", function () {
+        expect(reciprocal(2)).to.equal(0.5)
+        expect(reciprocal(4)).to.equal(0.25)
+    })
+})
